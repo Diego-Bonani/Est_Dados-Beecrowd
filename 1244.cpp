7303597bf71f4590ca1564596f0a136bacf0eb0e
Diego Gode Bonani <diegogode.bonani@gmail.com>
2025-04-21 15:49:53 -0300

    Add files via upload

---
+++ 1244.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+vector<string> separarStringPorEspaco(const string& str) {
+    vector<string> partes;
+    stringstream ss(str);
+    string palavra;
+
+    while (ss >> palavra) {
+        partes.push_back(palavra);
+    }
+
+    return partes;
+}
+
+void selectionSortDecrescente(vector<string>& partes) {
+    int n = partes.size();
+    for (int i = 0; i < n - 1; i++) {
+        int maiorIndice = i;
+        for (int j = i + 1; j < n; j++) {
+            if (partes[j].size() > partes[maiorIndice].size()) { // Compara pelo tamanho em ordem decrescente
+                maiorIndice = j;
+            }
+        }
+        if (maiorIndice != i) {
+            swap(partes[i], partes[maiorIndice]);
+        }
+    }
+}
+
+int main() {
+    string input;
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> input;
+        vector<string> resultado = separarStringPorEspaco(input);
+    
+        // Ordenar as partes pelo tamanho em ordem decrescente usando Selection Sort
+        selectionSortDecrescente(resultado);
+    
+        for (const string& parte : resultado) {
+            cout << parte << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
